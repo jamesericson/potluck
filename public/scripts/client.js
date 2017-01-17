281b2c307af03adb1db253190ca49e2be087df2f
James Ericson <jamesericson@gmail.com>
2017-01-17 13:06:39 -0600

    got it started. connected to DB

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,24 @@
 
-$(document).ready(function(){
-  console.log('JS & JQ');
 
-});
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'mainController', ['$scope', '$http', function($scope, $http){
+  console.log("NG");
+
+  $http({
+    method: 'GET',
+    url: '/foodOptions'
+  }).then(function(response){
+    console.log(response.data);
+    $scope.foodCategories = response.data;
+  });// end http
+
+  $scope.regester = function(){
+    if (!$scope.nameIn || !$scope.categoryIn){
+      alert('Fill out everything, please ;)');
+      return;
+    }; // end if
+    $scope.nameIn = null;
+    $scope.categoryIn = null;
+  }
+}]);// end mainController
--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var pg = require( 'pg' );
 var path = require('path');
 var app = express();
-var port = 3001;
+var port = process.env.PORT || 3001;
+//create a connection string to our database
+var connectionString = 'postgres://localhost:5432/potluckDB';
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -14,3 +17,28 @@ app.listen(port, function(){
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../public/view/index.html'));
 });
+
+app.get('/foodOptions', function(req, res){
+  console.log('hit get foodOptions');
+
+  pg.connect( connectionString, function(err, client, done){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log('connected to DB');
+      var query = client.query( 'SELECT * FROM foodOptions' );
+      //array for options
+      var allOptions = [];
+      query.on( 'row', function( row ){
+        allOptions.push (row);
+      });
+      query.on( 'end', function(){
+        done();
+        console.log( allOptions );
+
+        res.send( allOptions );
+      });
+    }// end if else
+  });//end connect
+});//end get foodOptions
